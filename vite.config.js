f1e682b9cecfe9bacd19e2323d1d2e128aabe4cf
Danu Prastyo <danuprastyo62@gmail.com>
2026-08-27 09:56:27 +0700

    Fix Netlify base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwind from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwind()],
-  base: '/tulearn-project/',
-})
+  base: mode === 'netlify' ? '/' : '/tulearn-project/',
+}))
