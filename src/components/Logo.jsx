8a394b84a8a781dbe9704d761eb2375bc5625be0
Danu Prastyo <danuprastyo62@gmail.com>
2026-08-27 08:52:21 +0700

    feat: update BASE_URL

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ function Logo({ className }) {
     return (
         <div>
             <a href="#" className="flex items-center justify-start gap-1.25">
-                <img src={`${import.meta.env.BASE_URL}assets/hero-img.png`} alt="Logo" className={className} />
+                <img src={`${import.meta.env.BASE_URL}assets/banner.png`} alt="Logo" className={className} />
                 <span className="text-2xl font-manrope font-bold text-primary">
                     Tu<span className="text-secondary">Learn</span>
                 </span>
--- src/constant/categories.js
+++ src/constant/categories.js
@@ -1,26 +1,26 @@
 export const CATEGORIES_INFORMATION = [
     {
-        image: '/assets/gif/Backend Icon.svg',
+        image: `${import.meta.env.BASE_URL}assets/gif/Backend Icon.svg`,
         title: 'Development',
         courses: '120+ Courses',
     },
     {
-        image: '/assets/gif/Google Icons _ My Business.svg',
+        image: `${import.meta.env.BASE_URL}assets/gif/Google Icons _ My Business.svg`,
         title: 'Business',
         courses: '80+ Courses',
     },
     {
-        image: '/assets/gif/Kaleidoscope Loading Animation.svg',
+        image: `${import.meta.env.BASE_URL}assets/gif/Kaleidoscope Loading Animation.svg`,
         title: 'Design',
         courses: '70+ Courses',
     },
     {
-        image: '/assets/gif/Bullhorn and social media icons for marketing.svg',
+        image: `${import.meta.env.BASE_URL}assets/gif/Bullhorn and social media icons for marketing.svg`,
         title: 'Marketing',
         courses: '60+ Courses',
     },
     {
-        image: '/assets/gif/Financial charts and statistics on tab with up arrow.svg',
+        image: `${import.meta.env.BASE_URL}assets/gif/Financial charts and statistics on tab with up arrow.svg`,
         title: 'Personal Growth',
         courses: '50+ Courses',
     },
